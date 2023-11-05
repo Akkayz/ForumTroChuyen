c792c938808ca789ab791aa551bf7c6354bfd0aa
Akkay <nguyenhung.dev.info@gmail.com>
2023-11-05 10:25:39 +0700

    Layout page "Đăng bài"

---
+++ WebTroChuyen/Controllers/DangBaiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTroChuyen.Controllers
+{
+    public class DangBaiController : Controller
+    {
+        // GET: DangBai
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
